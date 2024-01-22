0740caeb70ae3884099a30b7b27ddd4897f81315
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2024-01-23 03:35:32 +0600

    auth provider created

--- client/src/Provider/AuthProvider.jsx
+++ client/src/Provider/AuthProvider.jsx
@@ -3,7 +3,37 @@ import { createContext } from "react";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const value = {};
+  const loginWithEmail = async (email, password) => {
+    try {
+      const res = await axios.post("/login", { email, password });
+      return res.data;
+    } catch (err) {
+      return err;
+    }
+  };
+  const logout = async () => {
+    try {
+      const res = await axios.get("/logout");
+      return res.data;
+    } catch (err) {
+      return err;
+    }
+  };
+  const registerWithEmail = async (email, password, name, number, role) => {
+    try {
+      const res = await axios.post("/register", {
+        email,
+        password,
+        name,
+        number,
+        role,
+      });
+      return res.data;
+    } catch (err) {
+      return err;
+    }
+  };
+  const value = { loginWithEmail, logout, registerWithEmail };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
