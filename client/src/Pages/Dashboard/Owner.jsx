9a0b130c855d937852f22a108b240b004817c2b5
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2024-01-23 03:29:31 +0600

    dummy component creatd for renter & owner

---
+++ client/src/Pages/Dashboard/Owner.jsx
@@ -0,0 +1,9 @@
+const Owner = () => {
+  return (
+    <div>
+      <h3>Owner Dashboard</h3>
+    </div>
+  );
+};
+
+export default Owner;
---
+++ client/src/Pages/Dashboard/Renter.jsx
@@ -0,0 +1,9 @@
+const Renter = () => {
+  return (
+    <div>
+      <h3>Renter Dashboard</h3>
+    </div>
+  );
+};
+
+export default Renter;
